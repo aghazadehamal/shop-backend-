96f5e9a3bdc1a2022920f5292206c123265efa98
aghazadehamal@gmail.com <aghazadehamal@gmail.com>
2025-06-21 11:00:21 +0400

    new

--- controllers/carController.js
+++ controllers/carController.js
@@ -2,7 +2,7 @@ const pool = require('../db');
 
 // Elan əlavə et (şəkil ilə)
 exports.createCar = async (req, res) => {
-  const { marka, model, il, yurus, price, description } = req.body; // <-- yürüş → yurus
+const { marka, model, il, yurus, price, description, phone } = req.body;
   const userId = req.user.userId;
 const image_url = req.file ? req.file.path : null; // Cloudinary URL
 
@@ -10,9 +10,9 @@ const image_url = req.file ? req.file.path : null; // Cloudinary URL
 
   try {
     const result = await pool.query(
-      `INSERT INTO cars (marka, model, il, yurus, price, description, image_url, user_id)
-       VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *`,
-      [marka, model, il, yurus, price, description, image_url, userId]
+      `INSERT INTO cars (marka, model, il, yurus, price, description, image_url, user_id, phone)
+ VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
+[marka, model, il, yurus, price, description, image_url, userId, phone]
     );
 
     res.status(201).json(result.rows[0]);
@@ -67,17 +67,15 @@ exports.updateCar = async (req, res) => {
   const { id } = req.params;
   const userId = req.user.userId;
 
-  const { marka, model, il, yurus, price, description } = req.body; // <-- yürüş → yurus
- const image_url = req.file ? req.file.path : req.body.image_url;
-
-
+  const { marka, model, il, yurus, price, description, phone } = req.body;
+  const image_url = req.file ? req.file.path : req.body.image_url;
 
   try {
     const result = await pool.query(
       `UPDATE cars 
-       SET marka = $1, model = $2, il = $3, yurus = $4, price = $5, description = $6, image_url = $7
-       WHERE id = $8 AND user_id = $9 RETURNING *`,
-      [marka, model, il, yurus, price, description, image_url, id, userId]
+       SET marka = $1, model = $2, il = $3, yurus = $4, price = $5, description = $6, image_url = $7, phone = $8
+       WHERE id = $9 AND user_id = $10 RETURNING *`,
+      [marka, model, il, yurus, price, description, image_url, phone, id, userId]
     );
 
     if (result.rowCount === 0)
@@ -89,6 +87,7 @@ exports.updateCar = async (req, res) => {
     res.status(500).json({ message: "Yeniləmə zamanı xəta baş verdi" });
   }
 };
+
 
 
 
