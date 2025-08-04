519ceb3bde5edcbc0e08fbe4406013761b288d7d
aghazadehamal@gmail.com <aghazadehamal@gmail.com>
2025-08-04 12:35:28 +0400

    new

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const isRender = process.env.DB_URL?.includes('render.com'); 
+const isRender = process.env.DB_URL?.includes('render.com');
 
 const pool = new Pool({
   connectionString: process.env.DB_URL,
-  ...(isRender && {
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  }),
+  ssl: isRender
+    ? {
+        rejectUnauthorized: false,
+      }
+    : false, // lokal işləsin deyə
 });
 
 module.exports = pool;
